1e1b956e35db8b354636306f40171aacd252d39f
Expedito Andrade <expeditojazz@gmail.com>
2020-08-18 09:21:31 -0300

    add styling to inner jumbotron

---
+++ src/components/home/InnerJumbotrom.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import tw from 'tailwind.macro';
+
+const StyledInner = styled.div.attrs({
+  className: 'w-100 grid h-full',
+})`
+  .top {
+    ${tw`flex justify-between`}
+  }
+
+  .center {
+    ${tw`relative text-center`}
+    .title {
+      ${tw`relative`}
+      ::before {
+        content: '';
+        position: absolute;
+        border: 1px solid ${props => props.theme.white};
+        bottom: 0;
+        left: 0;
+        width: 100%;
+      }
+    }
+  }
+`;
+export default function InnerJumbotrom() {
+  return (
+    <StyledInner>
+      <div className="top">
+        <FontAwesomeIcon icon={faCoffee} />
+        <FontAwesomeIcon icon={faCoffee} />
+      </div>
+      <div className="center">
+        <div className="title">
+          <h3>Book A Job Interview with The best Companies</h3>
+        </div>
+        <div className="content">
+          <p>
+
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda amet doloremque nulla impedit qui incidunt unde sint aliquam perspiciatis optio et debitis, veniam officiis sit expedita accusantium consequuntur quos! Et.
+          </p>
+        </div>
+      </div>
+    </StyledInner>
+  );
+}
--- src/components/home/Jumbotron.js
+++ src/components/home/Jumbotron.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import InnerJumbotrom from './InnerJumbotrom';
 
 const StyledJumbotron = styled.div.attrs({
   className: 'w-100 px-64',
 })`
-  background: url('homeBackJumb.jpg');
-  height: 300px;
-  color: ${props => props.theme.green};
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
+background:
+  linear-gradient(
+    -8deg,
+    #a1c030e0,
+    #a1c030e0
+  ),
+  url('homeBackJumb.jpg');
+height: 300px;
+color: ${props => props.theme.white};
+background-size: cover;
+background-repeat: no-repeat;
+background-position: center;
 `;
 
 export default function Jumbotron() {
   return (
     <StyledJumbotron>
-      Hello from jumbotron
+      <InnerJumbotrom />
     </StyledJumbotron>
   );
 }
