911ef41f7279590385806a1496155dd777d702e2
Expedito Andrade <expeditojazz@gmail.com>
2020-08-18 07:40:33 -0300

    add background image to jumbotron

--- src/components/home/Jumbotron.js
+++ src/components/home/Jumbotron.js
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 
 const StyledJumbotron = styled.div.attrs({
   className: 'w-100 px-64',
-})``;
+})`
+  background: url('homeBackJumb.jpg');
+  height: 300px;
+  color: ${props => props.theme.green};
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
 
 export default function Jumbotron() {
   return (
