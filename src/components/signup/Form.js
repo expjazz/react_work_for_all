8f0ab779890ab13a7c6e8e91dd9ea091e7084a62
Expedito Andrade <expeditojazz@gmail.com>
2020-08-18 10:58:07 -0300

    add top right content to signup page

---
+++ src/components/signup/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+const StyledForm = styled.form.attrs({})``;
+const Form = () => (
+  <StyledForm>
+    hey
+  </StyledForm>
+);
+
+export default Form;
--- src/components/signup/Right.js
+++ src/components/signup/Right.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
+import Form from './Form';
 
-const StyledRight = styled.div.attrs({})``;
+const StyledRight = styled.div.attrs({})`
+& {
+  .top {
+    ${tw`flex justify-center content-center flex-col items-center`}
+    p {
+      ${tw`relative`}
+      :before {
+        content: '';
+        position: absolute;
+        border: 1px solid gray;
+        top: 50%;
+        left: 25px;
+        width: 100px;
+      }
+
+      ::after {
+        content: '';
+        position: absolute;
+        border: 1px solid gray;
+        top: 50%;
+        right: 25px;
+        width: 100px;
+      }
+    }
+  }
+}
+`;
 const Right = () => (
   <StyledRight>
-    rright
+    <div className="top">
+      <h3 className="title">Sign up to find your  dream job</h3>
+      <p>or</p>
+    </div>
+    <Form />
+
   </StyledRight>
 );
 
