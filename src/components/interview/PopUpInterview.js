d404232daa77d2862f4edc4823c220d7c24d5d05
Expedito Andrade <expeditojazz@gmail.com>
2020-08-21 17:58:03 -0300

    add popup primmary styling

--- src/components/interview/PopUpInterview.js
+++ src/components/interview/PopUpInterview.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
 
+const StyledPopUpInterview = styled.div.attrs({
+  className: 'bg-red-800 absolute',
+})`
+  height: 20rem;
+  width: 40rem;
+  top: 25%;
+  left: 25%;
+
+`;
 const PopUpInterview = ({ show }) => {
   const a = 'ts';
   return (
-    <div className={show ? 'opacity-100' : 'opacity-0'}>
-      sd
-    </div>
+    <StyledPopUpInterview>
+
+      <div className={show ? 'opacity-100' : 'opacity-0'}>
+        sd
+      </div>
+    </StyledPopUpInterview>
   );
 };
 
