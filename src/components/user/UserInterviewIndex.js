eaa1df3e9cb1cf3b9878059a872d3bc0ec33a163
Expedito Andrade <expeditojazz@gmail.com>
2020-08-22 09:34:16 -0300

    select interviews

--- src/components/user/UserInterviewIndex.js
+++ src/components/user/UserInterviewIndex.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
+import { useSelector } from 'react-redux';
+import interviewSelectors from '../../selectors/selectAllInterviews';
 
 const StyledUserInterviewIndex = styled.div.attrs({
   className: 'col-start-3 col-end-12 border-2 border-gray-300 rounded-lg mx-10',
 
 })``;
 const UserInterviewIndex = () => {
-  console.log('object');
+  const { selectCandidateInterviews } = interviewSelectors;
+  const allInterviews = useSelector(selectCandidateInterviews);
+  console.log(allInterviews);
   return (
     <StyledUserInterviewIndex>
       a
---
+++ src/selectors/selectAllInterviews.js
@@ -0,0 +1,16 @@
+import { createSelector } from 'reselect';
+
+const selectCandidateInterviews = createSelector(
+  state => state.users.interviews,
+  interviews => interviews.map(interview => ({
+    status: interview.status,
+    companyId: interview.company.id,
+    companyName: interview.company.name,
+    candidateId: interview.candidate.id,
+    candidateName: interview.candidate.name,
+    jobOfferId: interview.job_offer.id,
+    jobOfferPosition: interview.job_offer.position,
+  })),
+);
+
+export default { selectCandidateInterviews };
