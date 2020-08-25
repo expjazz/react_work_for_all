fd941d6dfba2f33708993c7e2868dd79ff5484f9
Expedito Andrade <expeditojazz@gmail.com>
2020-08-25 06:42:20 -0300

    add hover effect to interview setup

--- src/components/interview/IndexInterviewStatus.js
+++ src/components/interview/IndexInterviewStatus.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
 
-const IndexInterviewStatus = ({ image, classes, notImage }) => (
-  <div className={classes.cont}>
-    {notImage ? <img src={image} alt="" className={classes.img} /> : <div className={classes.img} style={{ background: image }} /> }
-  </div>
-);
+const StyledContainer = styled.div.attrs({})`
+position: relative;
+
+.text {
+  position: absolute;
+  ${tw`h-full w-full opacity-0 hover:opacity-100 bg-gray-600 text-white`}
+  :hover {
+    background: #000000a3;
+  }
+}
+`;
+
+const IndexInterviewStatus = ({
+  image, classes, notimage, infoOnHover,
+}) => (notimage ? (
+  <StyledContainer>
+    <div className="text transition-all duration-700 ">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, numquam?
+    </div>
+    <img src={image} alt="" className={`${classes.img} `} />
+  </StyledContainer>
+)
+  : (
+    <div className={`${classes.cont}`} style={{ backgroundColor: image }}>
+      <div className={classes.img} style={{ background: `${image}` }} />
+    </div>
+  ));
 
 export default IndexInterviewStatus;
 
 IndexInterviewStatus.propTypes = {
   image: PropTypes.string.isRequired,
   classes: PropTypes.objectOf(String).isRequired,
-  notImage: PropTypes.bool.isRequired,
+  notimage: PropTypes.bool.isRequired,
+  infoOnHover: PropTypes.oneOfType([
+    PropTypes.objectOf(String), PropTypes.bool,
+  ]).isRequired,
 };
--- src/components/user/UserInterviewIndex.js
+++ src/components/user/UserInterviewIndex.js
@@ -15,20 +15,27 @@ const UserInterviewIndex = () => {
   const { selectCandidateInterviews } = interviewSelectors;
   const allInterviews = useSelector(selectCandidateInterviews);
   const newArr = [];
-  const color = ['#97bf0f', '#ffb400', '#f1f1f1', '#ffffff', '#10bbb5', '#f72967'];
+  const color = ['#97bf0f', '#ffb400', '#10bbb5', '#f72967'];
   console.log(color);
   allInterviews.forEach(interview => {
-    const random = color[Math.floor(Math.random() * color.length)];
+    const random = Math.floor(Math.random() * color.length);
     newArr.push(interview);
     newArr.push(color[random]);
+    console.log(color[random]);
   });
-  console.log(allInterviews);
+  let count = 0;
+  console.log(newArr);
   if (allInterviews.length === 0) return <h3 className="col-start-3 col-end-12 border-2 border-gray-300 rounded-lg text-center pt-12 text-3xl w-full">No Interviews Scheduled</h3>;
   return (
     <StyledUserInterviewIndex>
       {newArr.map((interview, index) => {
-        const a = '2';
-        return <IndexInterviewStatus image={typeof interview === 'object' ? interview.candidateImage : interview} notImage={index % 2 === 0 ? true : null} classes={{ cont: 'w-full', img: 'w-full' }} />;
+        count += 1;
+        const infoOnHover = typeof interview === 'object' ? {
+          candidate: interview.candidateName,
+          position: interview.jobOfferPosition,
+          time: interview.time,
+        } : false;
+        return <IndexInterviewStatus key={count} infoOnHover={infoOnHover} image={typeof interview === 'object' ? interview.candidateImage : interview} notimage={index % 2 === 0} classes={{ cont: 'w-full', img: 'w-full h-full' }} />;
       })}
     </StyledUserInterviewIndex>
   );
